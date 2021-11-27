aa858d6216870a7e639ff1df7ea1db857f20e543
Rebekah <rbk1008@Gmail.com>
2021-11-27 15:32:54 -0700

    commit tree 08829a0c4a49c9e50997f6df4dab1b8b511212f4

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,13 @@
-import React from "react";
+ import React from "react";
 
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
+
+
